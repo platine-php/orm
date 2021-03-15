d8d379439bd8d88f269dac4cb899c6d25f786a16
Tony NGUEREZA <nguerezatony@gmail.com>
2021-03-15 20:29:43 +0100

    initial commit

---
+++ src/Entity.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+declare(strict_types=1);
+
+namespace Platine\Orm;
+
+/**
+ * Description of Entity
+ *
+ * @author tony
+ */
+class Entity
+{
+    //put your code here
+}
